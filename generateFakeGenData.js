35b78c61454a1905b0d8d04d7da74b2fdebcb271
CoolHandDev <mgshop1-coolhanddev@yahoo.com>
2016-05-09 11:19:37 -0400

    add generator for image

--- generateFakeGenData.js
+++ generateFakeGenData.js
@@ -24,7 +24,7 @@ User must pass in paramaters conversion type and number of rows to generate
 */
     process.argv.forEach(function(val, index, array) {
         if (array.length < 4) {
-            console.log('Error: Please pass the correct parameters using \'node index.js [conversiontype] [numberOfRows]\'');
+            console.log('Error: Please pass the correct parameters using \'node generateFakeGenData.js [conversiontype] [numberOfRows]\'');
         }
         if (index === 2) {
             conversionType = val;
@@ -61,13 +61,137 @@ if (conversionType.toLowerCase() === 'allergy') {
     console.log(generateAllergyGenData());
 }
 
+if (conversionType.toLowerCase() === 'image') {
+    console.log(generateImageGenData());
+}
+
 
 /*
     Generate the discrete data sets for each clinical type
 
  */
     function generateImageGenData() {
+        var imageFields = [
+            'src_patient_id', 'src_patient_lastname'
+            , 'src_patient_firstname', 'src_patient_middlename'
+            , 'src_provider_id', 'src_provider_dea_nbr'
+            , 'src_provider_lastname', 'src_provider_firstname'
+            , 'src_provider_middlename', 'src_location_id'
+            , 'src_location_name', 'encounter_datetime'
+            , 'src_doc_type', 'document_description'
+            , 'pages_in_file_count', 'logical_group_id'
+            , 'page_sequence_nbr', 'batch_name'
+            , 'file_path', 'scan_date'
+        ];
+
         var imageHeader = '';
+        var imageStream = fs.createWriteStream(faker.random.uuid() + '_ncs_convImg_GENImage.txt')
+
+        buildImageHeader();
+        //TODO: hard code for now. Implement config later so we can determine how to fake data based on fields
+        function buildImageDataRow() {
+            var imageDataString = '';
+            imageFields.forEach(function(currentValue, index, array) {
+                if (currentValue === 'src_patient_id') {
+                    imageDataString += faker.random.uuid();
+                }
+                if (currentValue === 'src_patient_lastname') {
+                    imageDataString += delimiter + faker.name.lastName();
+                }
+                if (currentValue === 'src_patient_firstname') {
+                    imageDataString += delimiter + faker.name.firstName();
+                }
+                if (currentValue === 'src_patient_middlename') {
+                    imageDataString += delimiter + faker.name.lastName();
+                }
+                if (currentValue === 'src_provider_id') {
+                    imageDataString += delimiter + faker.random.uuid();
+                }
+                if (currentValue === 'src_provider_dea_nbr') {
+                    imageDataString += delimiter + faker.random.number();
+                }
+                if (currentValue === 'src_provider_lastname') {
+                    imageDataString += delimiter + faker.name.lastName();
+                }
+                if (currentValue === 'src_provider_firstname') {
+                    imageDataString += delimiter + faker.name.firstName();
+                }
+                if (currentValue === 'src_provider_middlename') {
+                    imageDataString += delimiter + faker.name.lastName();
+                }
+                if (currentValue === 'src_location_id') {
+                    imageDataString += delimiter + faker.random.uuid();
+                }
+                if (currentValue === 'src_location_name') {
+                    imageDataString += delimiter + faker.company.companyName();
+                }
+                if (currentValue === 'encounter_datetime') {
+                    imageDataString += delimiter + (faker.date.past().getMonth() + 1) + '/' + (faker.date.past().getDate()) + '/' + (faker.date.past().getFullYear());
+                }
+                if (currentValue === 'src_doc_type') {
+                    imageDataString += delimiter + faker.random.word();
+                }
+                if (currentValue === 'document_description') {
+                    imageDataString += delimiter + faker.random.words();
+                }
+                if (currentValue === 'pages_in_file_count') {
+                    if(index % 2 === 0){
+                        imageDataString += delimiter + '1';
+                    } else if (index % 3 === 0) {
+                        imageDataString += delimiter + '4';
+                    } else {
+                        imageDataString += delimiter + '2';
+                    }
+                }
+                if (currentValue === 'logical_group_id') {
+                    imageDataString += delimiter + '0';
+                }
+                if (currentValue === 'page_sequence_number') {
+                    imageDataString += delimiter + '0';
+                }
+                if (currentValue === 'batch_name') {
+                    imageDataString += delimiter + faker.random.word();
+                }
+                if (currentValue === 'file_path') {
+                    imageDataString += delimiter + 'c:\\image_path\\' + faker.random.word();
+                }
+                if (currentValue === 'scan_date') {
+                    imageDataString += delimiter + (faker.date.past().getMonth() + 1) + '/' + (faker.date.past().getDate()) + '/' + (faker.date.past().getFullYear());
+                }
+            });
+            imageDataString += '\r\n';
+            return imageDataString;
+        };
+
+        imageStream.once('open', function(fd) {
+            imageStream.write(imageHeader);
+            imageStream.write('\r\n'); //new line before first line of data
+            for(var i = 0; i < numberofRows; i++) {
+                imageStream.write(buildImageDataRow());
+
+                if (i % updateFrequency === 0) {
+                    console.log(new Date() + ': # of rows written: ' + i);
+                }
+            }
+            console.log(new Date() + ': # of rows written: ' + i);
+
+            imageStream.end();
+        });
+
+
+
+        function buildImageHeader() {
+
+            imageFields.forEach(function(currentValue, index, array) {
+                if (index === 0) {
+                    imageHeader = currentValue
+                } else {
+                    imageHeader += delimiter + currentValue;
+                }
+            });
+        }
+
+        return imageHeader;
 
     }
 
